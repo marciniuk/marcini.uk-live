056392ede377da5b30e03d625eb6794849182a22
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2022-02-15 19:04:27 +0100

    jestem ślepy

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@ if (window.location.pathname === "/en/") {
     let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((t % (1000 * 60)) / 1000);
     document.querySelector("#wiek").innerHTML =
-      days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+      weeks + "w " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
     if (t < 0) {
       clearInterval(x);
       document.querySelector("#wiek").innerHTML =
@@ -112,7 +112,7 @@ if (window.location.pathname === "/pl/") {
     let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((t % (1000 * 60)) / 1000);
     document.querySelector("#web-dev").innerHTML =
-      days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+      weeks + "w " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
     if (t < 0) {
       clearInterval(x);
       document.querySelector("#web-dev").innerHTML = "*coś się zepsuło*";
@@ -150,7 +150,7 @@ if (window.location.pathname === "/pl/") {
     let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((t % (1000 * 60)) / 1000);
     document.querySelector("#wiek").innerHTML =
-      days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+      weeks + "w " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
     if (t < 0) {
       clearInterval(x);
       document.querySelector("#wiek").innerHTML =
